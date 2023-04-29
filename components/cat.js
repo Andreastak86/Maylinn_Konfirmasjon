4a83d85722fa5ae7599908f7617857bb4b98c243
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2023-04-29 21:10:25 +0200

    add rev

--- components/cat.js
+++ components/cat.js
@@ -1,48 +1,82 @@
-import { useState, useEffect } from "react";
-import Image from "next/image";
+// import { useState, useEffect } from "react";
 
-export default function CatPictures() {
-    const [pictureUrl, setPictureUrl] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+// export default function CatPictures() {
+//     const [pictureUrl, setPictureUrl] = useState("");
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [error, setError] = useState(null);
 
-    const fetchPicture = async () => {
-        setIsLoading(true);
+//     const fetchPicture = async () => {
+//         setIsLoading(true);
+//         try {
+//             const response = await fetch("https://randomfox.ca/floof/");
+//             const picture = await response.url;
+//             setPictureUrl(picture);
+//         } catch (error) {
+//             setError("An error occurred while fetching the data");
+//         } finally {
+//             setIsLoading(false);
+//         }
+//     };
+
+//     const handleClick = () => {
+//         fetchPicture();
+//     };
+
+//     useEffect(() => {
+//         fetchPicture();
+//     }, []);
+
+//     return (
+//         <div className=' flex flex-col items-center'>
+//             <h1 className=' text-white py-6 text-[20px]'>
+//                 Din daglige pusekatt
+//             </h1>
+//             {isLoading && <p>Loading data...</p>}
+//             {!isLoading && error && <p>{error}</p>}
+//             {!isLoading && !error && (
+//                 <img src={pictureUrl} alt='A cat' height='300' width='300' />
+//             )}
+//             <button
+//                 className=' bg-pink-400 my-6 p-2 rounded-md'
+//                 onClick={handleClick}
+//             >
+//                 Ny pusekatt
+//             </button>
+//         </div>
+//     );
+// }
+import React, { useState } from "react";
+
+function CatPictures() {
+    const [foxImageUrl, setFoxImageUrl] = useState(
+        "https://randomfox.ca/images/13.jpg"
+    );
+
+    const fetchNewFoxImage = async () => {
         try {
-            const response = await fetch("https://cataas.com/cat");
-            const picture = await response.url;
-            setPictureUrl(picture);
+            const response = await fetch("https://randomfox.ca/floof/");
+            const data = await response.json();
+            setFoxImageUrl(data.image);
         } catch (error) {
-            setError("An error occurred while fetching the data");
-        } finally {
-            setIsLoading(false);
+            console.error(error);
         }
     };
 
-    const handleClick = () => {
-        fetchPicture();
-    };
-
-    useEffect(() => {
-        fetchPicture();
-    }, []);
-
     return (
         <div className=' flex flex-col items-center'>
-            <h1 className=' text-white py-6 text-[20px]'>
-                Din daglige pusekatt
-            </h1>
-            {isLoading && <p>Loading data...</p>}
-            {!isLoading && error && <p>{error}</p>}
-            {!isLoading && !error && (
-                <img src={pictureUrl} alt='A cat' height='300' width='300' />
+            <h1 className=' text-white py-6 text-[20px]'>Hva med en rev?</h1>
+            <br />
+            {foxImageUrl !== "" && (
+                <img height={300} width={300} src={foxImageUrl} alt='Fox' />
             )}
             <button
                 className=' bg-pink-400 my-6 p-2 rounded-md'
-                onClick={handleClick}
+                onClick={fetchNewFoxImage}
             >
-                Ny pusekatt
+                Ny Rev
             </button>
         </div>
     );
 }
+
+export default CatPictures;
